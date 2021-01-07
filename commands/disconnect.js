3a2cd1325a2bdbb473f1b923bba0aa0104e30e9d
Andrew Kong <50548798+DMNight6@users.noreply.github.com>
2021-01-07 08:46:08 +0800

    Create disconnect.js

---
+++ commands/disconnect.js
@@ -0,0 +1,21 @@
+const { MessageEmbed } = require("discord.js");
+const { Player } = require("erela.js");
+
+module.exports = {
+    name: 'disconnect',
+    description: "Disconnect on requset.",
+    async execute(client, message, args) {
+        const player = message.client.Music.get(message.guild.id);
+        const embed = new MessageEmbed()
+            .setDescription("Bot disconnected on request")
+            .setFooter(`Requester : ${message.author.username}`, message.author.avatarURL())
+        
+        if (player) {
+            message.channel.send(embed)
+            player.destroy();
+        } else {
+            embed.setDescription("Unable to disconnect as Player is not detected in any VC of this guild!")
+            message.channel.send(embed)
+        }
+    }
+}
