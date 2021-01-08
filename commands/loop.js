ae4524571acf700617f436b76a3989b1d124724f
Andrew Kong <50548798+DMNight6@users.noreply.github.com>
2021-01-08 18:57:19 +0800

    Add loop command

---
+++ commands/loop.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: "loop",
+    description: "Loop track",
+    async execute(client, message, args) {
+        const player = message.client.Music.get(message.guild.id)
+        const embed = new MessageEmbed()
+            .setFooter(`Requester : ${message.author.username}`, message.author.avatarURL());
+
+        if (player && player.queue.current && player.textChannel == message.channel.id && !player.trackRepeat) {
+            embed.setDescription(`Enabled Track Loop`)
+            message.channel.send(embed)
+            player.setTrackRepeat(true)
+
+        } else if (player && player.queue.current && player.textChannel == message.channel.id && player.trackRepeat) {
+            embed.setDescription(`Disabled Track Loop`)
+            message.channel.send(embed)
+            player.setTrackRepeat(false)
+        } else {
+            embed.setDescription("Cannot set track repeat as:\n-Not playing\n-Not the blinded channel")
+            message.channel.send(embed)
+        }
+    }
+}
