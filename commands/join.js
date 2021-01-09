a59f14a394f25564aab1e1ed5085107fef2829c9
Andrew Kong <50548798+DMNight6@users.noreply.github.com>
2021-01-09 22:24:32 +0800

    Readd join

---
+++ commands/join.js
@@ -0,0 +1,42 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "join",
+    description: "Join to the VC you want! (This overwrites binded text and voice channel)",
+    async execute(client, message, args) {
+        let player = message.client.Music.get(message.guild.id);
+        const channel = message.member.voice.channel;
+        const embed = new MessageEmbed()
+            .setDescription(`Joined ${channel.name}`);
+
+        if (!player) {
+            player = await client.Music.create({
+                guild: message.guild.id,
+                voiceChannel: message.member.voice.channel.id,
+                textChannel: message.channel.id,
+                selfDeafen: true,
+                selfMute: false,
+            })
+
+            if (player.state != 'CONNECTED') player.connect();
+            message.channel.send(embed)
+        } else if(player) {
+            player.destroy();
+
+            player = await client.Music.create({
+                guild: message.guild.id,
+                voiceChannel: message.member.voice.channel.id,
+                textChannel: message.channel.id,
+                selfDeafen: true,
+                selfMute: false,
+            })
+            
+            
+            if (player.state != 'CONNECTED') player.connect(message.member.voice.channel);
+            message.channel.send(embed)
+        }
+
+
+    
+        }
+}
